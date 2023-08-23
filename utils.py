0407d738c308bb2e9eecd2bcfa77ceda543e0526
Francesco Vaselli <francesco.vaselli@protonmail.com>
2023-08-23 15:57:29 +0200

    val funcs7

--- utils.py
+++ utils.py
@@ -82,8 +82,8 @@ class ClassificationMetrics(tf.keras.callbacks.Callback):
         #     epoch % 5 != 4
         # ):  # Here we check if the epoch is a multiple of 5 (adjust if 0-indexing is confusing)
         #     return
-        y_pred = self.model.predict(self.val_data[0])
-        y_true = self.val_data[1]
+        y_pred = self.model.predict(self.val_data)
+        y_true = np.concatenate([y.numpy() for _, y in self.val_data.unbatch()], axis=0)
 
         true_label, pred_label, fpr, tpr, roc_auc = check_classification(
             y_true, y_pred, self.threshold

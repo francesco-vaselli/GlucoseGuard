33dad2d06e39a25faba8b45ad97206de9526c340
Francesco Vaselli <francesco.vaselli@protonmail.com>
2023-08-24 15:48:20 +0200

    attn net

--- train_model.py
+++ train_model.py
@@ -6,6 +6,8 @@ import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv1D, Flatten, Dense, LSTM
 from tensorflow.keras.callbacks import TensorBoard
+from tensorflow.keras import layers
+from tensorflow import keras
 import datetime
 
 from utils import (
@@ -13,6 +15,46 @@ from utils import (
     ClassificationMetrics,
     filter_stationary_sequences_dataset,
 )
+
+def transformer_encoder(inputs, head_size, num_heads, ff_dim, dropout=0):
+    # Normalization and Attention
+    x = layers.LayerNormalization(epsilon=1e-6)(inputs)
+    x = layers.MultiHeadAttention(
+        key_dim=head_size, num_heads=num_heads, dropout=dropout
+    )(x, x)
+    x = layers.Dropout(dropout)(x)
+    res = x + inputs
+
+    # Feed Forward Part
+    x = layers.LayerNormalization(epsilon=1e-6)(res)
+    x = layers.Conv1D(filters=ff_dim, kernel_size=1, activation="relu")(x)
+    x = layers.Dropout(dropout)(x)
+    x = layers.Conv1D(filters=inputs.shape[-1], kernel_size=1)(x)
+    return x + res
+
+def build_attn_model(
+    input_shape,
+    output_shape,
+    head_size,
+    num_heads,
+    ff_dim,
+    num_transformer_blocks,
+    mlp_units,
+    dropout=0,
+    mlp_dropout=0,
+):
+    inputs = keras.Input(shape=input_shape)
+    x = inputs
+    for _ in range(num_transformer_blocks):
+        x = transformer_encoder(x, head_size, num_heads, ff_dim, dropout)
+
+    x = layers.GlobalAveragePooling1D(data_format="channels_first")(x)
+    for dim in mlp_units:
+        x = layers.Dense(dim, activation="relu")(x)
+        x = layers.Dropout(mlp_dropout)(x)
+    outputs = layers.Dense(output_shape)
+    return keras.Model(inputs, outputs)
+
 
 
 class FeedBack(tf.keras.Model):
@@ -129,6 +171,19 @@ def build_model(model_config):
         print("AR-RNN model built:", "\n")
         # feedback_model.summary()
         return feedback_model
+    elif model_config["model_type"] == "attn":
+        attn_config = model_config["attn_config"]
+        model = build_attn_model(
+            input_shape=attn_config["input_shape"],
+            output_shape=attn_config["output_shape"],
+            head_size=attn_config["head_size"],
+            num_heads=attn_config["num_heads"],
+            ff_dim=attn_config["ff_dim"],
+            num_transformer_blocks=attn_config["num_transformer_blocks"],
+            mlp_units=attn_config["mlp_units"],
+            dropout=attn_config["dropout"],
+            mlp_dropout=attn_config["mlp_dropout"],
+        )
     else:
         raise NotImplementedError(f"{model_config['model_type']} not implemented")
 

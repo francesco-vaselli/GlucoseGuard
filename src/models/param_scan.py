6be03ada99ef92383cf47f6db9d8a5ac2caf3880
Francesco Vaselli <francesco.vaselli@protonmail.com>
2023-08-25 17:15:30 +0200

    att optim3

--- src/models/param_scan.py
+++ src/models/param_scan.py
@@ -10,8 +10,7 @@ from tensorflow.keras import layers
 from tensorflow import keras
 import datetime
 import keras_tuner as kt
-
-from src.utils import (
+from ..src.utils import (
     CustomImageLogging,
     ClassificationMetrics,
     filter_stationary_sequences_dataset,

13b0cddb79eea990d431d49d62919088de6d0997
Francesco Vaselli <francesco.vaselli@protonmail.com>
2024-02-25 12:51:51 +0100

    new ds optimizationj

--- src/models/param_scan.py
+++ src/models/param_scan.py
@@ -16,6 +16,40 @@ from ..utils import (
     filter_stationary_sequences_dataset,
 )
 
+def multi_label_classification(y_true, data_mean, data_std):
+    """if for last y in y_true.shape[1]
+    y < 80, then [1, 0, 0]
+    if 80 <= y < 180, then [0, 1, 0]
+    if y >= 180, then [0, 0, 1]
+
+
+    Args:
+        y_true (np.array): array of shape (N, 6)
+    """
+    y_target = np.zeros((y_true.shape[0], 3))
+    # invert the normalization
+    y_true = y_true * data_std + data_mean
+    # apply the conditions
+    y_target = np.where(y_true[:, -1] < 80, [1, 0, 0], y_target)
+    y_target = np.where(y_true[:, -1] >= 80, [0, 1, 0], y_target)
+    y_target = np.where(y_true[:, -1] >= 180, [0, 0, 1], y_target)
+
+    return y_target
+
+def two_label_classification(y_true, data_mean, data_std):
+    """if for last y in y_true
+    y < 80, then 0
+    if y >= 80, then 1
+
+    Args:
+        y_true (np.array): array of shape (N, 6)
+    """
+    y_true = y_true * data_std + data_mean
+    y_target = np.zeros((y_true.shape[0], 1))
+    y_target = np.where(y_true[:, -1] < 80, 0, y_target)
+    y_target = np.where(y_true[:, -1] >= 80, 1, y_target)
+    return y_target
+
 def transformer_encoder(inputs, head_size, num_heads, ff_dim, dropout=0):
     # Normalization and Attention
     x = layers.LayerNormalization(epsilon=1e-6)(inputs)
@@ -32,7 +66,7 @@ def transformer_encoder(inputs, head_size, num_heads, ff_dim, dropout=0):
     x = layers.Conv1D(filters=inputs.shape[-1], kernel_size=1)(x)
     return x + res
 
-def build_transformer_model(hp):
+def build_transformer_model(hp, target="regression"):
     head_size = hp.Int("head_size", min_value=32, max_value=512, step=32)
     num_heads = hp.Int("num_heads", min_value=1, max_value=8, step=1)
     ff_dim = hp.Int("ff_dim", min_value=32, max_value=512, step=32)
@@ -52,18 +86,29 @@ def build_transformer_model(hp):
     for _ in range(mlp_layers):
         x = layers.Dense(mlp_dim, activation="relu")(x)
         x = layers.Dropout(dropout)(x)
-    outputs = layers.Dense(6)(x)
+    if target == "regression":
+        outputs = layers.Dense(6)(x)
+    elif target == "classification":
+        outputs = layers.Dense(1, activation="sigmoid")(x)
+    elif target == "multi_classification":
+        outputs = layers.Dense(3, activation="softmax")(x)
+
     model = keras.Model(inputs, outputs)
 
     optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
 
-    loss = tf.keras.losses.MeanAbsoluteError()
+    if target == "regression":
+        loss = tf.keras.losses.MeanAbsoluteError()
+    elif target == "classification":
+        loss = tf.keras.losses.BinaryCrossentropy()
+    elif target == "multi_classification":
+        loss = tf.keras.losses.CategoricalCrossentropy()
 
     model.compile(optimizer=optimizer, loss=loss)
 
     return model
 
-def build_cnn_model(hp):
+def build_cnn_model(hp, target="regression"):
     model = Sequential()
 
     filter_units = hp.Int("filter_units", min_value=32, max_value=512, step=32)
@@ -101,19 +146,29 @@ def build_cnn_model(hp):
         model.add(Dense(dense_units, activation="relu"))
 
     # Add output layer
-    model.add(Dense(6))
+    if target == "regression":
+        model.add(Dense(6))
+    elif target == "classification":
+        model.add(Dense(1, activation="sigmoid"))
+    elif target == "multi_classification":
+        model.add(Dense(3, activation="softmax"))
     # print("CNN model built:", "\n", model.summary())
 
     optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
 
-    loss = tf.keras.losses.MeanAbsoluteError()
+    if target == "regression":
+        loss = tf.keras.losses.MeanAbsoluteError()
+    elif target == "classification":
+        loss = tf.keras.losses.BinaryCrossentropy()
+    elif target == "multi_classification":
+        loss = tf.keras.losses.CategoricalCrossentropy()
 
     model.compile(optimizer=optimizer, loss=loss)
 
     return model
 
 
-def build_rnn_model(hp):
+def build_rnn_model(hp, target="regression"):
     model = Sequential()
 
     rnn_units = hp.Int("rnn_units", min_value=32, max_value=512, step=32)
@@ -139,7 +194,12 @@ def build_rnn_model(hp):
         model.add(Dense(dense_units, activation="relu"))
 
     # Add output layer
-    model.add(Dense(6))
+    if target == "regression":
+        model.add(Dense(6))
+    elif target == "classification":
+        model.add(Dense(1, activation="sigmoid"))
+    elif target == "multi_classification":
+        model.add(Dense(3, activation="softmax"))
 
     optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
     loss = tf.keras.losses.MeanAbsoluteError()
@@ -150,6 +210,8 @@ def build_rnn_model(hp):
 
 def train(
     data_path,
+    data_mean,
+    data_std,
     n_train,
     n_val,
     n_test,
@@ -161,6 +223,7 @@ def train(
     learning_rate,
     model_config,
     model_type,
+    target,
 ):
     # 1. Load the data
     ds = np.load(data_path)
@@ -172,6 +235,16 @@ def train(
     test_x = ds[n_train + n_val : n_train + n_val + n_test, :7]
     test_y = ds[n_train + n_val : n_train + n_val + n_test, 7:]
     print("train_x shape:", train_x.shape)
+
+    # if target is classification, transform the target
+    if target == "classification":
+        train_y = two_label_classification(train_y, data_mean, data_std)
+        val_y = two_label_classification(val_y, data_mean, data_std)
+        test_y = two_label_classification(test_y, data_mean, data_std)
+    elif target == "multi_classification":
+        train_y = multi_label_classification(train_y, data_mean, data_std)
+        val_y = multi_label_classification(val_y, data_mean, data_std)
+        test_y = multi_label_classification(test_y, data_mean, data_std)
 
     # Ensure that train_x has the right shape [samples, timesteps, features]
     # for the moment this stays
@@ -203,7 +276,7 @@ def train(
 
     if model_type == "cnn":
         tuner = kt.BayesianOptimization(
-            build_cnn_model,
+            lambda hp: build_cnn_model(hp, target=target),
             objective="val_loss",
             max_trials=100,
             num_initial_points=None,
@@ -214,7 +287,7 @@ def train(
         )
     elif model_type == "rnn":
         tuner = kt.BayesianOptimization(
-            build_rnn_model,
+            lambda hp: build_rnn_model(hp, target=target),
             objective="val_loss",
             max_trials=100,
             num_initial_points=None,
@@ -225,7 +298,7 @@ def train(
         )
     elif model_type == "transformer":
         tuner = kt.BayesianOptimization(
-            build_transformer_model,
+            lambda hp: build_transformer_model(hp, target=target),
             objective="val_loss",
             max_trials=100,
             num_initial_points=None,
@@ -251,6 +324,9 @@ def train(
     best_hps = tuner.get_best_hyperparameters(num_trials=1)[0]
     best_hps1 = tuner.results_summary(num_trials=1)
 
+    # save the best hyperparameters to a yaml file for each model_type and target
+    with open(f"{model_type}_{target}_best_hps.yaml", "w") as file:
+        yaml.dump(best_hps.values, file)
 
     print(
         f"""
@@ -265,39 +341,44 @@ def main():
     with open("configs/train_config.yaml", "r") as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--model_type", type=str, default="cnn", help="Type of model to train."
-    )
-    args = parser.parse_args()
-    model_type = args.model_type
+    models = ["cnn", "rnn", "transformer"]
+    target = ["regression", "classification", "multi_classification"]
 
-    data_path = config["data_path"]
-    n_train = config["n_train"]
-    n_val = config["n_val"]
-    n_test = config["n_test"]
-    batch_size = config["batch_size"]
-    buffer_size = config["buffer_size"]
-    epochs = config["epochs"]
-    optimizer = config["optimizer"]
-    loss = config["loss"]
-    learning_rate = config["learning_rate"]
-    model_config = config["model_config"]
+    for model_type in models:
+        model_type = model_type
+        for target_type in target:
+            target = target_type
+            
+            data_path = config["data_path"]
+            data_mean = config["data_mean"]
+            data_std = config["data_std"]
+            n_train = config["n_train"]
+            n_val = config["n_val"]
+            n_test = config["n_test"]
+            batch_size = config["batch_size"]
+            buffer_size = config["buffer_size"]
+            epochs = config["epochs"]
+            optimizer = config["optimizer"]
+            loss = config["loss"]
+            learning_rate = config["learning_rate"]
+            model_config = config["model_config"]
 
-    train(
-        data_path,
-        n_train,
-        n_val,
-        n_test,
-        batch_size,
-        buffer_size,
-        epochs,
-        optimizer,
-        loss,
-        learning_rate,
-        model_config,
-        model_type,
-    )
+            train(
+                data_path,
+                data_mean,
+                data_std,
+                n_train,
+                n_val,
+                n_test,
+                batch_size,
+                buffer_size,
+                epochs,
+                optimizer,
+                loss,
+                learning_rate,
+                model_config,
+                model_type,
+            )
 
 
 if __name__ == "__main__":
